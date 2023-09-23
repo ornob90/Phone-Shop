e2fe4328000699a13409fd19871f4670853016ce
ornob90 <oornob49@gmail.com>
2023-09-23 14:35:36 +0600

    nav bar design complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const navBtns = ["Home", "Favorites", "Login"];
+
+  return (
+    <div className="max-w-[1440px] mx-auto">
+      <nav className="pt-3 mx-auto flex justify-between w-[80%]">
+        <h1 className="font-bold text-3xl">Phone Shop</h1>
+
+        <ul className="flex justify-center items-center gap-8">
+          {navBtns.map((btn) => (
+            <NavLink className="font-medium text-xl" key={btn}>
+              {btn}
+            </NavLink>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
