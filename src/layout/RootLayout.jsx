b97d44ebf827f8d294f4d52414d08ec6123e6cf6
ornob90 <oornob49@gmail.com>
2023-09-23 14:27:51 +0600

    root router setup with outlet complete

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
-  return <div className="text-black">RootLayout</div>;
+  return (
+    <div className="text-black">
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
 
 export default RootLayout;
--- src/route/route.jsx
+++ src/route/route.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout></RootLayout>,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
